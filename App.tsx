87b34da53242fdd58a5d14084592d4766bebe068
Long Truong <long_truong@datahosue.com>
2023-03-01 14:26:00 +0700

    initial updates - fix fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import { Tabs } from './navigation/tabs';
 import { HomeScreen, RestaurantScreen, OrderDeliveryScreen } from './screens';
 
 const Stack = createStackNavigator();
 
 export const App = () => {
+  const [loaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
+  });
+
+  if(!loaded){
+    return null
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator
